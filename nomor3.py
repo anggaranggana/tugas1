d498e2cd3e8fcaabf5773c243daa0338a16a4756
Angga Ranggana Putra <anggaranggana@gmail.com>
2021-04-23 23:32:18 +0700

    nomor tiga bingung

---
+++ nomor3.py
@@ -0,0 +1,10 @@
+teori = float(input("berapa nilai ujian teori anda"))
+praktik = float(input("berapa nilai ujian praktik anda"))
+if teori == praktik:
+    print(lulus)
+elif teori != praktik:
+    print(ulang ujian)
+else teori > praktik:
+    print(ulang ujian praktik)
+
+saya bingung yang ini tolong ajari lagi
