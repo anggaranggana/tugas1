755ef806363228f54da9a5dd90c2eaf0115bfd1a
Angga Ranggana Putra <anggaranggana@gmail.com>
2021-04-23 18:58:10 +0700

    revisi

---
+++ nomor2.py
@@ -0,0 +1,5 @@
+a = 8
+b = 9
+
+if b > a:
+    print("anda pintar anjing")
---
+++ nomor3.py
@@ -0,0 +1,11 @@
+a = 70
+b = 70
+c = 75
+d = 65
+
+if a == b:
+    print ("selamat anda lulus")
+elif a > c:
+    print("selamat anda lulus")
+else a > d:
+    print("gak tau")
