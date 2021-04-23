cabc4a9ba775b20a490238565972b881b7a808f7
Angga Ranggana Putra <anggaranggana@gmail.com>
2021-04-23 19:39:39 +0700

    revisi lagi

--- nomor1.py
+++ nomor1.py
@@ -1,3 +1,4 @@
-#name = input("enter name:")
-#print("your name is:" + name)
-
+nama = input("Nama:")
+umur = input("Umur:")
+tinggi = input("tinggi badan:")
+print (("Nama Saya " + nama + ","),("Umur saya " + umur),(" dan tinggi badan saya " + tinggi))
