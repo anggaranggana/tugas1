0a2eef038fd4ac7e9c410d23a339193d84bd5898
Angga Ranggana Putra <anggaranggana@gmail.com>
2021-04-23 17:10:21 +0700

    nomor1

--- nomor1.py
+++ nomor1.py
@@ -1,6 +1,8 @@
-x = 2
-y = "cinta kita"
-
-print (y)
-
-print (x + 29)
+a =  "Angga"
+b = 30
+c = 175
+d = (a,b,c)
+txt1 = "Nama saya {}".format(a)
+txt2 = "Usia saya {}".format(b)
+txt3 = "Tinggi badan saya {}".format(c)
+print(txt1,txt2,txt3)
